0459cfdd089d776680958331da7b2baaae7030ae
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-10-06 13:09:13 +0545

    Console highlighter

---
+++ src/ua/com/juja/study/core/sqlcmd/util/StringHighlighter.java
@@ -0,0 +1,19 @@
+package ua.com.juja.study.core.sqlcmd.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viktor
+ * Date: 10/4/14
+ * Time: 2:33 PM
+ */
+public class StringHighlighter {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+}

1a8d68e9ee871368614ae7dea364d8c6f4f75dc1
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-10-07 19:26:08 +0545

    moved labs to another package

--- src/ua/com/juja/study/core/labs/juja_is_awesome/JujaAwesome.java
+++
@@ -1,8 +0,0 @@
-package ua.com.juja.study.core.labs.juja_is_awesome;
-
-public class JujaAwesome {
-
-    public static void printJujaIsAwesome() {
-        System.out.println("uja is awesome!");
-    }
-}

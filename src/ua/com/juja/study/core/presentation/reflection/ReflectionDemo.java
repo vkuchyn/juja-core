6a98ec2cd604771c060b472ab31040ad700e9d5e
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-12-11 10:08:47 +0700

    Lesson 14 etc

--- src/ua/com/juja/study/core/presentation/reflection/ReflectionDemo.java
+++ src/ua/com/juja/study/core/presentation/reflection/ReflectionDemo.java
@@ -1,10 +1,6 @@
 package ua.com.juja.study.core.presentation.reflection;
 
-import ua.com.juja.study.core.presentation.annotations.AnnotationsDemo;
 import ua.com.juja.study.core.presentation.annotations.Juja;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,7 +23,7 @@ public class ReflectionDemo {
         return "ReflectionDemo" + String.valueOf(field1);
     }
 
-    private static class A extends ReflectionDemo{
+    private static class A extends ReflectionDemo {
         @Override
         public String toString() {
             return "A" + String.valueOf(field1);
